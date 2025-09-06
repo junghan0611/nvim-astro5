2a05746c29ff1fd58ce8266bc8c3374e5b36f548
junghan0611 <31724164+junghan0611@users.noreply.github.com>
2025-09-06 20:13:36 +0900

    bugfix

--- lua/plugins/termux.lua
+++ lua/plugins/termux.lua
@@ -13,41 +13,42 @@ if not termux then return {} end
 -- ------------------------------------------
 
 -- ------------------------------------------
-------@type LazySpec
----return {
----  -- {
----  --   "WhoIsSethDaniel/mason-tool-installer.nvim",
----  --   -- INFO: prevent Mason loading Clojure & Lua LSP servers
----  --   -- overrides `require("mason-tool-installer").setup(...)`
----  --   -- Language server names found in `:Mason`
----  --   opts = {
----  --     ensure_installed = {
----  --       -- Conditional install of language servers
----  --       "lua-language-server",
----  --       "clojure-lsp",
----  --       condition = function() return not termux end,
----  --     },
----  --   },
----  -- },
----  -- {
----  --   "AstroNvim/astrolsp",
----  --   ---@type AstroLSPOpts
----  --   -- INFO: Use local Clojure & Lua LSP servers
----  --   opts = {
----  --     -- Configuration table of features provided by AstroLSP
----  --     features = {
----  --       autoformat = false, -- enable or disable auto formatting on start
----  --       codelens = false, -- enable/disable codelens refresh on start
----  --       inlay_hints = false, -- enable/disable inlay hints on start
----  --       semantic_tokens = true, -- enable/disable semantic token highlighting
----  --     },
----  --     -- Use local servers (not mason installed)
----  --     servers = {
----  --       -- "pyright"
----  --       "clojure-lsp",
----  --       "lua-language-server",
----  --     },
----  --   },
----  -- },
----}
+---@type LazySpec
+return {}
+-- return {
+--   {
+--     "WhoIsSethDaniel/mason-tool-installer.nvim",
+--     -- INFO: prevent Mason loading Clojure & Lua LSP servers
+--     -- overrides `require("mason-tool-installer").setup(...)`
+--     -- Language server names found in `:Mason`
+--     opts = {
+--       ensure_installed = {
+--         -- Conditional install of language servers
+--         "lua-language-server",
+--         "clojure-lsp",
+--         condition = function() return not termux end,
+--       },
+--     },
+--   },
+--   {
+--     "AstroNvim/astrolsp",
+--     ---@type AstroLSPOpts
+--     -- INFO: Use local Clojure & Lua LSP servers
+--     opts = {
+--       -- Configuration table of features provided by AstroLSP
+--       features = {
+--         autoformat = false, -- enable or disable auto formatting on start
+--         codelens = false, -- enable/disable codelens refresh on start
+--         inlay_hints = false, -- enable/disable inlay hints on start
+--         semantic_tokens = true, -- enable/disable semantic token highlighting
+--       },
+--       -- Use local servers (not mason installed)
+--       servers = {
+--         -- "pyright"
+--         "clojure-lsp",
+--         "lua-language-server",
+--       },
+--     },
+--   },
+-- }
 -- ------------------------------------------
